d18f76aa919d1063bf19e0b797ddec42e427b0ff
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-05 18:03:49 +0100

    add tag filter functionality

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,9 +5,10 @@ require 'dm-migrations'
 class Tag
   include DataMapper::Resource
 
+  has n, :links, through: Resource
+
   property :id, Serial
   property :name, String
 
-  # has n, :links, :through => Resource
 
 end
