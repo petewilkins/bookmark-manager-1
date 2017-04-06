8f9a8bdcbb2de91ad175e7cc1e09d82a0292eaf6
emmpak <eiliev05@yahoo.com>
2017-04-06 14:32:47 +0100

    Add password confirmation

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,8 +3,19 @@ feature 'Sign Up' do
     visit '/users/new'
     fill_in 'email', with: "roi@ma.com"
     fill_in 'password', with: "password"
+    fill_in 'password_confirmation', with: 'password'
     click_button "Sign Up"
     expect(current_path).to eq '/links'
     expect(page).to have_content "Welcome, roi@ma.com"
+  end
+
+  scenario 'user enters mismatching passwords' do
+    visit '/users/new'
+    fill_in 'email', with: "roi@ma.com"
+    fill_in 'password', with: "password"
+    fill_in 'password_confirmation', with: 'password2'
+    click_button "Sign Up"
+    expect(page).not_to have_content "Welcome, roi@ma.com"
+    expect(User.count).to eq 0
   end
 end
